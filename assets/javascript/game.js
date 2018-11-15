428d8ce9a683eab7d3d47d3c1e3e8c03b649b647
Sam Swerdling <you@example.com>
2018-11-14 22:07:13 -0700

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,83 @@
-var x = 0;
-        var y = {};
-        var click1 = "0";
-        var computerNumber = 0;
-        var userNumber = "";
-        var wins;
-        var losses = 0;
-        var resetFunction;
+var click1 = "0";
+var computerNumber = 0;
+var userNumber = "";
+var wins = 0;
+var losses = 0;
+var resetFunction;
+var gameEnd = false;
 
-        function reset() {
-            click1 = "0";
-            computerNumber = getRandomNum(19, 120);
-            userNumber = 0;
-
-            $("#userCrystal").text(userNumber);
-            $("#computerCrystal").text(computerNumber);
-            return computerNumber;
-            return userNumber;
-
-        }
-        function getRandomNum(min, max) {
-
-            return Math.floor(Math.random() * (max - min)) + min;
-
-        }
+function reset() {
+    click1 = "0";
+    computerNumber = getRandomNum(19, 120);
+    userNumber = 0;
+    gameEnd = false;
+    $("#userCrystal").text(userNumber);
+    $("#computerCrystal").text(computerNumber);
+    removeDiv();
+    setTimeout(makeDivs(), 20);
+    setTimeout(Game(), 30);
 
 
-        function makeDivs() {
-            var imageLink = ["assets/images/GemGreen.png", "assets/images/GemBlue.png", "assets/images/GemPurple.png", "assets/images/GemRed.png"]
-            var array = [1, 2, 3, 4];
-            for (var i = 0; i < array.length; i++) {
-                var div = $("<button>");
-                div.addClass('crystal');
-                div.attr('value', (Math.floor(Math.random() * 11)) + 1);
-                div.attr('id', i);
-                $('.divContainer').append(div);
 
-                var img = $("<img>");
-                img.addClass('image');
-                img.attr('src', imageLink[i]);
-                $('#' + i).append(img);
+}
 
-            }
-        }
-        function high() {
-            losses = losses++;
-            $("#loss").text(losses);
+function getRandomNum(min, max) {
 
-            setTimeout(reset, 2000);
-            return losses;
+    return Math.floor(Math.random() * (max - min)) + min;
 
-        }
-        function low() {
-
-        }
-        function win() {
-            wins++;
-            $("#wins").text(wins);
-        }
-
-        makeDivs();
-        reset();
+}
 
 
-        $(".crystal").on('click', function () {
+function makeDivs() {
+    var imageLink = ["assets/images/GemGreen.png", "assets/images/GemBlue.png", "assets/images/GemPurple.png", "assets/images/GemRed.png"]
+    var array = [1, 2, 3, 4];
+    for (var i = 0; i < array.length; i++) {
+        var div = $("<button>");
+        div.addClass('crystal');
+        div.attr('value', (Math.floor(Math.random() * 11)) + 1);
+        div.attr('id', i);
+        $('.divContainer').append(div);
 
-            click1 = $(this).val();
+        var img = $("<img>");
+        img.addClass('image');
+        img.attr('src', imageLink[i]);
+        $('#' + i).append(img);
+
+    }
+}
+
+function removeDiv() {
+    $(".crystal").remove();
+    $(".image").remove();
+}
+
+function high() {
+    gameEnd = true;
+    losses++;
+    $("#loss").text(losses);
+
+    setTimeout(reset, 2000);
+
+}
+
+
+function win() {
+    gameEnd = true;
+    wins++;
+    $("#wins").text(wins);
+    setTimeout(reset, 2000);
+}
+
+
+reset();
+
+function Game() {
+    $(".crystal").on('click', function () {
+
+        click1 = $(this).val();
+        if (gameEnd == false) {
+
             userNumber = parseInt(userNumber) + parseInt(click1);
-            console.log(click1);
-            console.log($(this));
             $("#userCrystal").text(userNumber);
 
             console.log(userNumber)
@@ -81,9 +91,10 @@ var x = 0;
             else if (parseInt(userNumber) == parseInt(computerNumber)) {
                 win();
             }
-            else if (parseInt(userNumber) < parseInt(computerNumber)) {
-                low();
+            else {
+
             }
 
-
-        });
+        }
+    });
+}
